c184c6f586f79c5ecd3b9eca049ae29f115fb2d7
emmy <52672675+emmaccode@users.noreply.github.com>
2025-05-26 16:31:41 -0600

    added `docs` tab

--- src/OliveDocBrowser.jl
+++ src/OliveDocBrowser.jl
@@ -2,13 +2,17 @@ module OliveDocBrowser
 using Olive.Toolips
 using Olive.ToolipsSession
 using Olive
-using Olive.ToolipsMarkdown
-using Olive: getname, Project, build_tab, open_project, Directory, Cell, build_tab
-import Olive: build
+using Olive.ToolipsServables
+using Olive: getname, Project, build_tab, open_project, Directory, Cell
+import Olive: build, build_tab, style_tab_closed!
 
-build(c::Connection, om::OliveModifier, oe::OliveExtension{:docbrowser}) = begin
+build(c::Connection, om::Olive.OliveModifier, oe::Olive.OliveExtension{:docbrowser}) = begin
     explorericon = Olive.topbar_icon("docico", "newspaper")
     on(c, explorericon, "click") do cm::ComponentModifier
+        if "doctab" in cm
+            olive_notify!(cm, "you already have documentation open, you cannot open two docbrowsers at once.", color = "red")
+            return
+        end
         mods = [begin 
             if :mod in keys(p.data)
                 p.data[:mod]
@@ -18,11 +22,14 @@ build(c::Connection, om::OliveModifier, oe::OliveExtension{:docbrowser}) = begin
         end for p in c[:OliveCore].open[getname(c)].projects]
         filter!(x::Any -> ~(isnothing(x)), mods)
         push!(mods, Olive, c[:OliveCore].olmod)
-        cells = Vector{Cell}([Cell(e, "docmodule", "", mod) for (e, mod) in enumerate(mods)])
+        cells = Vector{Cell}([Cell("docmodule", "", mod) for mod in mods])
+        for mod in mods
+            
+        end
         home_direc = Directory(c[:OliveCore].data["home"])
         projdict::Dict{Symbol, Any} = Dict{Symbol, Any}(:cells => cells,
-        :path => home_direc.uri, :env => home_direc.uri)
-        myproj::Project{:doc} = Project{:doc}(home_direc.uri, projdict)
+        :path => home_direc.uri, :env => home_direc.uri, :pane => "one")
+        myproj::Project{:doc} = Project{:doc}("docs", projdict)
         push!(c[:OliveCore].open[getname(c)].projects, myproj)
         tab::Component{:div} = build_tab(c, myproj)
         Olive.open_project(c, cm, myproj, tab)
@@ -32,6 +39,53 @@ end
 
 function build(c::Connection, cm::ComponentModifier, cell::Cell{:docdirectory}, proj::Project{<:Any})
 
+end
+
+function build_tab(c::Connection, p::Project{:doc}; hidden::Bool = false)
+    fname::String = p.id
+    tabbody::Component{:div} = div("tab$(fname)", class = "tabopen", 
+    style = "background-color:#216934;")
+    if(hidden)
+        tabbody[:class]::String = "tabclosed"
+    end
+    tablabel::Component{:a} = a("tablabel$(fname)", text = p.name, class = "tablabel", 
+    style = "color:white;")
+    push!(tabbody, tablabel)
+    on(c, tabbody, "click") do cm::ComponentModifier
+        projects::Vector{Project{<:Any}} = c[:OliveCore].open[getname(c)].projects
+        inpane = findall(proj::Project{<:Any} -> proj[:pane] == p[:pane], projects)
+        [begin
+            if projects[e].id != p.id 
+                style_tab_closed!(cm, projects[e])
+            end
+            nothing
+        end  for e in inpane]
+        projbuild::Component{:div} = build(c, cm, p)
+        set_children!(cm, "pane_$(p[:pane])", [projbuild])
+        cm["tab$(fname)"] = :class => "tabopen"
+        if length(p.data[:cells]) > 0
+            focus!(cm, "cell$(p[:cells][1].id)")
+        end
+    end
+    on(c, tabbody, "dblclick") do cm::ComponentModifier
+        if "$(fname)dec" in cm
+            return
+        end
+        decollapse_button::Component{:span} = span("$(fname)dec", text = "arrow_left", class = "tablabel")
+        on(c, decollapse_button, "click") do cm2::ComponentModifier
+            remove!(cm2, "$(fname)close")
+            remove!(cm2, "$(fname)add")
+            remove!(cm2, "$(fname)restart")
+            remove!(cm2, "$(fname)run")
+            remove!(cm2, "$(fname)switch")
+            remove!(cm2, "$(fname)dec")
+        end
+        style!(decollapse_button, "color" => "blue")
+        controls::Vector{<:AbstractComponent} = Olive.tab_controls(c, p)
+        insert!(controls, 1, decollapse_button)
+        [begin append!(cm, tabbody, serv); nothing end for serv in controls]
+    end
+    tabbody::Component{:div}
 end
 
 function build(c::Connection, cm::ComponentModifier, cell::Cell{:docmodule}, proj::Project{<:Any})
@@ -49,7 +103,7 @@ function build(c::Connection, cm::ComponentModifier, cell::Cell{:docmodule}, pro
         end
         docdiv
     end for name in n]
-    mainbox[:children] = vcat([h("$(cell.outputs)", 2, text = string(cell.outputs))], selectorbuttons)
+    mainbox[:children] = vcat([h2("$(cell.outputs)", text = string(cell.outputs))], selectorbuttons)
     mainbox
 end
 end # module OliveDocBrowser
